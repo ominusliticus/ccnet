91f433536e14ce026ae7c9cb9043645ef6433c4d
Kevin Ingles <elkevin111@gmail.com>
2025-06-21 17:45:14 -0400

    Add test that checks if lapack behaves as expected

--- tests/test_lapackpp.cpp
+++ tests/test_lapackpp.cpp
@@ -1,12 +1,15 @@
 #include <lapack.hh>
 
 int main() {
-    std::complex<double> mat[3 * 3] = {
-        { 1.0, 0.0 }, { 0.0, 0.0 }, { 0.0, 0.0 },
-        { 0.0, 0.0 }, { 1.0, 0.0 }, { 0.0, 0.0 },
-        { 0.0, 0.0 }, { 0.0, 0.0 }, { 1.0, 0.0 }
-    };
-    int64_t pivot[3] = { 1, 2, 3 };
-    int64_t return_code = lapack::getrf2(3, 3, mat, 3, pivot);
+    float a[] = { 12.0f, 6.0f, -4.0f, -51.0f, 167.0f, 24.0f, 4.0f, -68.0f, -41.0f };
+    float t[3];
+    int64_t return_code = lapack::geqr2(3, 3, a, 3, t);
+    std::cout << return_code << "\n";
+    for (int i = 0; i < 3; ++i)
+    {
+        for (int j = 0; j < 3; ++j)
+            std::cout << "\t" << a[i + 3 * j] << " ";
+         std::cout << "\n";
+    }
     return return_code;
 }
